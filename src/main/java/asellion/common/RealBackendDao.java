95bf78302b7dff3839a81558dfd325a7c4a94225
Pablo Mosteiro <pmosteiro@tripadvisor.com>
2019-10-24 17:49:31 +0100

    explain how the database-access object will be constructed

--- src/main/java/asellion/common/RealBackendDao.java
+++ src/main/java/asellion/common/RealBackendDao.java
@@ -1,45 +1,37 @@
 package asellion.common;
 
-import java.io.UnsupportedEncodingException;
 import java.util.List;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
 import asellion.manager.AsellionManager;
 
+/**
+ * This class will contain the real implementation of the database-access object, which will talk to the database to save and retrieve entries
+ * It will be built using JDBC
+ */
 public class RealBackendDao implements BackendDao {
-    private final Algorithm algorithm; // inject the algorithm so that we can choose various variants
-    private final JWTVerifier verifier; // inject so we can choose variants
+    private static final String ERROR_MESSAGE = "Not implemented yet";
 
-    private static final int TOKEN_VERSION = 1; // This means the Jwt implementation that we are starting with
+    RealBackendDao() {
 
-    RealBackendDao(String secretSignature) {
-        try {
-            this.algorithm = Algorithm.HMAC512(secretSignature);
-            this.verifier = JWT.require(algorithm).build();
-        } catch (UnsupportedEncodingException e) {
-            throw new IllegalStateException(e.getMessage(), e);
-        }
     }
 
     @Override
     public List<AsellionManager.ProductResponse> getAllProducts() {
-        return null;
+        throw new IllegalStateException(ERROR_MESSAGE);
     }
 
     @Override
     public AsellionManager.ProductResponse getProduct(int id) {
-        throw new IllegalStateException("Not implemented yet");
+        throw new IllegalStateException(ERROR_MESSAGE);
     }
 
     @Override
     public void updateProduct(int id, AsellionManager.ProductRequest request) {
-        throw new IllegalStateException("Not implemented yet");
+        throw new IllegalStateException(ERROR_MESSAGE);
     }
 
     @Override
     public void createProduct(AsellionManager.ProductRequest request) {
-        throw new IllegalStateException("Not implemented yet");
+        throw new IllegalStateException(ERROR_MESSAGE);
     }
 }
